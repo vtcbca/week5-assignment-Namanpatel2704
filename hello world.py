1402d26cff9bc4ce5976b07f20a6e9f23e4b6a75
Namanpatel2704 <141392150+Namanpatel2704@users.noreply.github.com>
2023-08-17 14:40:05 +0530

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import.csv
+
+
+# In[2]:
+
+
+import.csv
+
+
+# In[3]:
+
+
+import csv
+
+
+# In[7]:
+
+
+with open('c:/sqlite3/csv/HELLO WORLD.txt','r',errors="ignore")as f:
+    contents =f.read()
+    print(contents)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
